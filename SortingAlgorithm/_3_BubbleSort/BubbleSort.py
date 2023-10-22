e0893885ee7f7161446a89d4a8340648d5aff49b
Mahmoud-Namnam <148398760+MahmoudNamNam@users.noreply.github.com>
2023-10-22 19:32:54 +0300

    Update BubbleSort.py

--- SortingAlgorithm/_3_BubbleSort/BubbleSort.py
+++ SortingAlgorithm/_3_BubbleSort/BubbleSort.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def bubbleSort(arr):
     n = len(arr)
 
@@ -26,32 +25,3 @@ if __name__ == "__main__":
     print("Sorted array:")
     for i in range(len(arr)):
         print("%d" % arr[i], end=" ")
-=======
-def bubbleSort(arr):
-    n = len(arr)
-
-    for i in range(n):
-        swapped = False
-
-        for j in range(0, n - i - 1):
-
-            # Traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        if (swapped == False):
-            break
-
-
-# Driver code to test above
-if __name__ == "__main__":
-    arr = [*map(int, input().split())]
-
-    bubbleSort(arr)
-
-    print("Sorted array:")
-    for i in range(len(arr)):
-        print("%d" % arr[i], end=" ")
->>>>>>> 88fee41232b9dc675c16e5a5d7e1e499f37dcd53
