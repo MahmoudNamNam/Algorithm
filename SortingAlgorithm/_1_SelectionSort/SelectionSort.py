aa29b077657fc29c38fe004f2a85dd39d295c7f9
Mahmoud-Namnam <148398760+MahmoudNamNam@users.noreply.github.com>
2023-10-22 19:30:21 +0300

    Update SelectionSort.py

--- SortingAlgorithm/_1_SelectionSort/SelectionSort.py
+++ SortingAlgorithm/_1_SelectionSort/SelectionSort.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 A = [*map(int, input().split())]
 
 
@@ -20,26 +20,4 @@ for i in range(len(A)):
 print("Sorted array")
 for i in range(len(A)):
     print("%d" % A[i], end=" , ")
-=======
-A = [*map(int, input().split())]
 
-
-
-for i in range(len(A)):
-
-    # Find the minimum element in remaining
-    # unsorted array
-    min_idx = i
-    for j in range(i + 1, len(A)):
-        if A[min_idx] > A[j]:
-            min_idx = j
-
-    # Swap the found minimum element with
-    # the first element
-    A[i], A[min_idx] = A[min_idx], A[i]
-
-# Driver code to test above
-print("Sorted array")
-for i in range(len(A)):
-    print("%d" % A[i], end=" , ")
->>>>>>> 88fee41232b9dc675c16e5a5d7e1e499f37dcd53
