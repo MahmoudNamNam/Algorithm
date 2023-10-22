a99423a0d5ccc001c66d0c51f611664fc349cea2
Mahmoud-Namnam <148398760+MahmoudNamNam@users.noreply.github.com>
2023-10-22 19:32:39 +0300

    Update BubbleSort.java

--- SortingAlgorithm/_3_BubbleSort/BubbleSort.java
+++ SortingAlgorithm/_3_BubbleSort/BubbleSort.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Algorithm.SortingAlgorithm._3_BubbleSort;
 
 import java.util.Arrays;
@@ -41,47 +40,3 @@ public class BubbleSort {
         System.out.println(Arrays.toString(array));
     }
 }
-=======
-package Algorithm.SortingAlgorithm._3_BubbleSort;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class BubbleSort {
-
-
-    public void sort(int[] arr) {
-        int n = arr.length;
-        boolean swapped;
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            // If no two elements were swapped in the inner loop, then the array is already sorted
-            if (!swapped)
-                break;
-
-        }
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the size of the array: ");
-        int size = scanner.nextInt();
-        int[] array = new int[size];
-        System.out.println("Enter " + size + " elements:");
-        for (int i = 0; i < size; i++) {
-            array[i] = scanner.nextInt();
-        }
-        BubbleSort bubbleSort = new BubbleSort();
-        bubbleSort.sort(array);
-        System.out.println(Arrays.toString(array));
-    }
-}
->>>>>>> 88fee41232b9dc675c16e5a5d7e1e499f37dcd53
