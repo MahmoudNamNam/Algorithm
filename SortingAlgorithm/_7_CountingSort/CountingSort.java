e2ef7dd8eaa2647180a5a42a176c0e09bb971d82
Mahmoud-Namnam <148398760+MahmoudNamNam@users.noreply.github.com>
2023-10-22 23:25:04 +0300

    Update CountingSort.java

--- SortingAlgorithm/_7_CountingSort/CountingSort.java
+++ SortingAlgorithm/_7_CountingSort/CountingSort.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package Algorithm.SortingAlgorithm._7_CountingSort;
 
 import java.util.*;
@@ -49,55 +49,4 @@ public class CountingSort {
         }
     }
 }
-=======
-package Algorithm.SortingAlgorithm._7_CountingSort;
 
-import java.util.*;
-
-
-public class CountingSort {
-
-    public static int[] countSort(int[] inputArray) {
-        // Finding the maximum element in inputArray
-        int max = Arrays.stream(inputArray).max().getAsInt();
-
-        // Initializing countArray with 0
-        int[] countArray = new int[max + 1];
-
-        // Mapping each element of inputArray as an index of countArray
-        for (int num : inputArray) {
-            countArray[num]++;
-        }
-
-        // Calculating prefix sum at every index of countArray
-        for (int i = 1; i <= max; i++) {
-            countArray[i] += countArray[i - 1];
-        }
-
-        // Creating outputArray from countArray
-        int[] outputArray = new int[inputArray.length];
-
-        for (int i = inputArray.length - 1; i >= 0; i--) {
-            outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
-            countArray[inputArray[i]]--;
-        }
-
-        return outputArray;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        int[] inputArray =new int[n];
-        for (int i=0; i<n; i++)
-            inputArray[i]= scanner.nextInt();
-        int[] outputArray = countSort(inputArray);
-
-        System.out.println("Sorted Array:");
-        for (int num : outputArray) {
-            System.out.print(num + " ");
-        }
-    }
-}
->>>>>>> 88fee41232b9dc675c16e5a5d7e1e499f37dcd53
